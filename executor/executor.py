ddd27763fcc67cf328b5d9f318665fe0d617b348
Francisco J. Guzman <36437485+guzmanfj@users.noreply.github.com>
2021-11-24 08:20:24 +0300

    Added package files

---
+++ executor/executor.py
@@ -0,0 +1,220 @@
+import subprocess
+import os
+from subprocess import CalledProcessError
+import tempfile
+import shutil
+import time
+import logging
+
+from pathlib import Path
+
+class RunError(Exception):
+    """
+    Base class for exceptions
+    """
+    pass
+
+
+class Executor:
+    """
+    Class to make calls to external programs
+
+    Create a subclass of Executor for running your programs. The methods that
+    you may want to override are:
+    
+    - `__init__` ... to set your own default values (might want to call parent __init__ too)
+    - `prepare` ... this one is called BEFORE program execution, best used to prepare input files
+    - `finish` ... called AFTER the successful program execution, to process output as needed
+    - `isFailed` ... for situations where the program ran successfully (exited with code 0), but there is still some unwanted output
+
+    Once you instantiate the class with the appropriate arguments, you can run
+    the program with the run method. Example to run the 'ls' command in the
+    current directory:
+
+        >>> exe = Executor(['ls', '.'], catch_out=False)
+        >>> args, returncode, stdout = exe.run() 
+    """
+
+    def __init__(self, args:list, catch_out:bool=False, tempdir:Path=None,
+        keep_tempdir:bool=False, cwd:Path=Path.cwd(), verbose:bool=True):
+        """
+        Initialize the necessary variables to run the program. Override, but
+        call parent method with super().__init__(...)
+
+        Args:
+            args (list): 
+                List with the command name and arguments, as it would be run
+                in the command line. Only a single program can be run. If you
+                need to redirect the output somewhere, take the 
+                self.completed_process.stdout variable and use that accordingly
+            catch_out (bool or str, optional): 
+                Whether or not to catch the stdout from the program. If True,
+                creates an output file called {program}.out in the cwd and saves
+                stdout to that file. If a string is provided, it will be used
+                as a file name for the output file. Defaults to True.
+            tempdir (str, optional): 
+                Name for the directory to create temporary input/output files.
+                Defaults to None.
+            keep_tempdir (bool, optional): 
+                Whether or not to keep the temporary folder. If False, it will
+                be deleted in the cleanup() method. Defaults to False.
+            cwd (str, optional): Working directory to run the program in, and
+                to save the stdout if catch_out=True. Defaults to '.'.
+            verbose (bool, optional): Prints progress messages and error message
+                from fail() to the terminal. Defaults to True.
+        """
+        self.program = args[0]
+        self.args = args
+        self.catch_out = catch_out
+        self.keep_tempdir = keep_tempdir
+        self.cwd = cwd
+        self.tempdir = tempdir
+        self.verbose = verbose
+
+        if self.verbose:
+            logging.basicConfig(format='%(levelname)s:%(message)s',
+                level=logging.INFO)
+
+        if catch_out == True:
+            self.f_stdout = os.path.join(self.cwd, f'{self.program}.out')
+        elif isinstance(catch_out, str):
+            self.f_stdout = catch_out
+
+    def prepare(self):
+        """
+        Prepare input for program (e.g. create input files).
+        OVERRIDE, especially if you don't need to create a temporary directory
+        for your input/output
+        """
+        logging.info('Creating temporary directory...')
+        if self.tempdir:
+            if not os.path.isdir(self.tempdir):
+                os.mkdir(self.tempdir)
+        else:
+            self.tempdir = Path(tempfile.mkdtemp(
+                prefix=self.program+'_'+self.__class__.__name__.lower()+'_'))
+
+
+    def execute(self):
+        """
+        Run external command and block until it is finished
+        """
+        start_time = time.time()
+        
+        p = subprocess.run(self.args, stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT, text=True, cwd=self.cwd)
+        
+        runtime = time.time() - start_time
+
+        return runtime, p
+
+
+    def run(self):
+        """
+        Method that will be called for running the external program
+        """
+        self.completed_process = None
+        self.failed_message = None
+        try:
+            self.prepare()
+
+            logging.info(f'Running command:\n$ {" ".join(self.args)}')
+            self.runtime, self.completed_process = self.execute()
+
+            # Check if exit code of process was 0
+            self.completed_process.check_returncode()
+
+            # Check for other errors/not desired output even if exit code is 0
+            self.failed_message = self.isFailed()
+            if self.failed_message:
+                self.error = RunError
+                self.fail()
+            else:
+                logging.info('Command was run successfully.')
+                logging.info(f'stdout:\n{self.completed_process.stdout}')
+                self.result = self.finish()
+
+        except OSError as e:
+            raise RunError(
+                f"Couldn't run or communicate with external program: {e.strerror}")
+
+        except (MemoryError, CalledProcessError) as e:
+            self.error = e
+            self.fail()
+
+        finally:
+            self.cleanup()
+        
+        return self.result
+
+
+    def fail(self):
+        """
+        Generates the error message and raises the corresponding error if the
+        program fails.
+        """
+        error_string = \
+            f"\n{self.program} EXECUTION FAILED.\n"+\
+            f"Command: {' '.join(self.args)}\n"
+        if self.completed_process:
+            error_string += \
+                f"Returncode: {self.completed_process.returncode}\n"+\
+                f"stdout: \n"+\
+                self.completed_process.stdout
+        
+        if self.verbose:
+            logging.error(error_string)
+
+        if self.catch_out:
+            with open(self.f_stdout, 'w') as f:
+                f.write(error_string)
+
+        if self.failed_message:
+            raise self.error(self.failed_message)
+        else:
+            raise self.error
+
+
+    def isFailed(self):
+        """
+        Not needed in most cases. Only override when the program may produce
+        unwanted output even if it runs successfully (the exit code is 0). 
+        Check for the output and return your own error message, be
+        as descriptive as you wish.
+        """
+        undesired_output_present = False
+        if undesired_output_present:
+            return "The output is not as expected."
+        else:
+            return False
+
+
+    def finish(self):
+        """
+        OVERRIDE to process output as necessary, e.g. save to output file or
+        parse into pandas DataFrame.
+        Be sure to save output in a location other than the temporary directory,
+        which will be removed in the cleanup() method.
+        """
+        if self.catch_out:
+            with open(self.f_stdout, 'w') as f:
+                f.write(self.completed_process.stdout)
+
+        result = (
+                self.completed_process.args,
+                self.completed_process.returncode,
+                self.completed_process.stdout
+        )
+
+        return result
+
+
+    def cleanup(self):
+        """
+        Remove temporary directory after external program has finished
+        (failed or not)
+        """
+        if self.tempdir and self.tempdir.exists():
+            if not self.keep_tempdir:
+                logging.info('Removing temporary directory...')
+                shutil.rmtree(self.tempdir)
---
+++ test/executor_test.py
@@ -0,0 +1,141 @@
+import unittest
+import os, sys, tempfile, shutil
+from subprocess import CalledProcessError
+from pathlib import Path
+
+sys.path.append(os.fspath(Path(Path(__file__).parent.parent, 'executor')))
+
+from executor import Executor
+from executor import RunError
+
+class ExecutorTest(unittest.TestCase):
+    """
+    Class for testing the executor module
+    """
+
+    def setUp(self):
+        self.args = ['ls', '.']
+        self.program = self.args[0]
+        self.tempdir = Path(tempfile.gettempdir(),
+            'ls_'+self.__class__.__name__.lower())
+        self.cwd = self.tempdir
+
+    def test_createsTempdir(self):
+        """
+        Creates a temp directory when no tempdir is specified
+        """
+        self.exe = Executor(self.args, catch_out=False,
+            tempdir=None, keep_tempdir=True, verbose=False)
+        self.exe.run()
+        
+        self.assertTrue(self.exe.tempdir.exists())
+
+    def test_createsSpecifiedTempdir(self):
+        """
+        Creates the specified tempdir
+        """
+        self.assertFalse(self.tempdir.exists())
+
+        self.exe = Executor(self.args, catch_out=False,
+            tempdir=self.tempdir, keep_tempdir=True, verbose=False)
+        self.exe.run()
+        
+        self.assertTrue(self.tempdir.exists())
+
+    def test_removesTempdir(self):
+        """
+        Removes tempdir after execution
+        """
+        self.exe = Executor(self.args, catch_out=False,
+            tempdir=None, keep_tempdir=False, verbose=False)
+        self.exe.run()
+
+        self.assertFalse(self.exe.tempdir.exists())
+
+    def test_invalid_program(self):
+        """
+        Raises RunError when called with an invalid program
+        """
+        args = ['l', '.']
+        self.exe = Executor(args, catch_out=False,
+            tempdir=None, keep_tempdir=False, verbose=False)
+        
+        with self.assertRaises(RunError):
+            self.exe.run()
+
+    def test_invalid_arguments(self):
+        """
+        Raises CalledProcessError when called with invalid arguments and
+        exit code is not zero
+        """
+        args = ['ls', 'hello']
+        self.exe = Executor(args, catch_out=False, tempdir=None,
+            keep_tempdir=False, verbose=False)
+
+        with self.assertRaises(CalledProcessError):
+            self.exe.run()
+
+    def test_custom_error_message(self):
+        """
+        Raises RunError with custom error message if provided in isFailed()
+        """
+        error_message="The output is not as expected"
+        
+        class FailingProgram(Executor):
+            def isFailed(self):
+                return error_message
+        
+        self.exe = FailingProgram(self.args, catch_out=False, tempdir=None,
+            keep_tempdir=False, verbose=False)
+
+        with self.assertRaises(RunError) as cm:
+            self.exe.run()
+
+        exception = cm.exception
+        self.assertEqual(exception.args[0], error_message)
+
+    def test_ls_output(self):
+        """
+        Test the output of the `ls` program
+        """
+        self.tempdir.mkdir()
+        open(Path(self.tempdir, 'test1.txt'), 'w').close()
+        open(Path(self.tempdir, 'test2.txt'), 'w').close()
+        open(Path(self.tempdir, 'test3.txt'), 'w').close()
+
+        self.exe = Executor(self.args, catch_out=False, tempdir=self.tempdir,
+            keep_tempdir=True, cwd=self.cwd, verbose=False)
+        self.out = self.exe.run()
+
+        self.assertEqual(len(self.out), 3)
+        self.assertEqual(self.out[0], ['ls', '.'])
+        self.assertEqual(self.out[1], 0)
+        self.assertEqual(self.out[2], 'test1.txt\ntest2.txt\ntest3.txt\n')
+
+    def test_catch_ls_output(self):
+        """
+        Catch the output of the 'ls' program correctly
+        """
+        self.tempdir.mkdir()
+        open(Path(self.tempdir, 'test1.txt'), 'w').close()
+        open(Path(self.tempdir, 'test2.txt'), 'w').close()
+        open(Path(self.tempdir, 'test3.txt'), 'w').close()
+        
+        self.exe = Executor(self.args, catch_out=True, tempdir=self.tempdir,
+            keep_tempdir=True, cwd=self.cwd, verbose=False)
+        self.out = self.exe.run()
+
+        f_out = Path(self.cwd, f'{self.program}.out')
+        self.assertTrue(os.path.isfile(f_out))
+
+        with open(f_out, 'r') as f:
+            contents = f.read()
+        self.assertEqual(contents, 'test1.txt\ntest2.txt\ntest3.txt\n')
+
+    def tearDown(self):
+        if self.exe.tempdir.exists():
+            shutil.rmtree(self.exe.tempdir)
+
+
+if __name__ == '__main__':
+    unittest.main()
